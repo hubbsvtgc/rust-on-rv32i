4b7a63087cf2681f9c1281e11e5a03250b25045f
Vincent <bsvtgc@gmail.com>
2022-09-07 20:03:11 -0400

    Initial setup for bare metal build Create custom linker file Remove main fn and add start entry Set stack pointer Map trap handler Set .cargo/config to build for rv32imac

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use core::arch::asm;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
+#[no_mangle]
+#[link_section = ".mtvec_base"]
+ fn trap_handler() -> ! {
+    loop{}
+}
+
+#[no_mangle]
+pub fn _start() -> ! {
+
+    extern "C" {
+        static  _stack_start: u32;
+    }
+
+    type FnPtr = fn() -> !;
+    let th: FnPtr = trap_handler;
+
+    unsafe{
+
+        let sp = &_stack_start;
+
+        asm!("csrw mtvec, {}" ,
+            in(reg) th);
+
+        asm!("mv sp, {}" ,
+            in(reg) sp);
+    }
+
+    loop {}
 }
