156abe6ec3b54203891d49ff579b70e6cb32905a
Vincent <bsvtgc@gmail.com>
2022-10-02 20:10:38 -0400

    Blue led blink code from RAM but messy code

--- src/bin.rs
+++ src/bin.rs
@@ -2,11 +2,13 @@
 #![no_main]
 
 extern crate hifive1_revb_board;
-
-use hifive1_revb_board::GpioMmapRegs;
-
 use core::panic::PanicInfo;
 use core::arch::asm;
+
+const BLUE_LED_GPIO: u8 = 21;
+const PLIC_BASE: u32 = 0xC000000; // 0xC00 << 16 = 0xC00 0000,  
+const PLIC_CLAIMCOMP_CTX1_OFFSET: u32 = 0x200004;
+const MIE_SET: u32 = 0;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -14,33 +16,91 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[no_mangle]
-#[link_section = ".mtvec_base"]
- fn trap_handler() -> ! {
-    loop{}
+
+ fn trap_handler() {
+
+    unsafe{
+        asm!("mv t0, {}", in(reg) PLIC_BASE);
+        asm!("mv t1, {}", in(reg) PLIC_CLAIMCOMP_CTX1_OFFSET);
+        asm!("add t0, t1, t0");
+        asm!("lw t1, 0(t0)");
+        asm!("sw t1, 0(t0)");
+        asm!("mret");
+    }
+
+/*
+    li t0, PLIC_BASE;
+li t1, PLIC_CLAIMCOMP_CTX1_OFFSET;
+add t0, t1, t0;
+
+lw t1, 0(t0);
+sw t1, 0(t0);
+slli t1, t1, 2;
+*/
+
+    //loop{}
 }
 
-#[no_mangle]
-pub fn _start() -> ! {
-
-    let gpio_cfg  = 0x1001_2000 as *mut GpioMmapRegs;
-
+fn set_stack(){
     extern "C" {
         static  _stack_start: u32;
     }
-
-    type FnPtr = fn() -> !;
-    let th: FnPtr = trap_handler;
-
     unsafe{
-
-        let sp = &_stack_start;
-
-        asm!("csrw mtvec, {}" ,
-            in(reg) th);
-
-        asm!("mv sp, {}" ,
-            in(reg) sp);
+        let sp1 = &_stack_start;
+        asm!("mv sp, {}", in(reg) sp1);
     }
+}
 
-    loop {}
+fn set_trap_handler()
+{
+    type FnPtr = fn();
+    let th: FnPtr = trap_handler;
+    unsafe{
+        asm!("csrw mtvec, {}", in(reg) th);
+    }
+}
+
+fn clear_external_interrupt()
+{
+    /* for now disable external interrupt to avoid mtvec */
+    unsafe {
+        asm!("csrr t0, mie");
+        asm!("mv t1, {}", in(reg) MIE_SET);
+        //asm!("or t0, t0, t1"); - this enables the external interrupt
+        asm!("csrw mie, t1");
+    }
+}
+
+#[no_mangle]
+
+#[link_section = ".entry"]
+pub extern "C" fn _start() -> ! {
+
+    set_stack();
+    set_trap_handler();
+    clear_external_interrupt();
+
+    hifive1_revb_board::GpioPin::configure_as_out(BLUE_LED_GPIO);
+
+    loop {
+
+        // set high 1 - already pulleup so blue led on
+        hifive1_revb_board::GpioPin::write_low(BLUE_LED_GPIO);
+
+        let mut  delay:  u32 = 0xfffff;
+
+        /* delay */
+        while delay > 0 {
+            delay -=  1
+        } 
+        
+        // set high 1 - blue led off (since pulledup) 
+        hifive1_revb_board::GpioPin::write_high(BLUE_LED_GPIO);
+
+        delay = 0xfffff;
+
+        while delay > 0 {
+            delay  -=  1
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,130 @@
 //! interface for using peripherals in board.
 #![no_std]
 
+pub enum Dir {
+    In,
+    Out,
+}
+
+pub enum Func {
+    Gpio,
+    Iof,
+}
+
+pub struct GpioPin;
 
 #[repr(C)]
-pub struct GpioMmapRegs{
-    pub iput_val: u32, /* Pin Value */
-    pub iput_async_en: u32, /* Pin input enable */
-    pub oput_en: u32, /* pin output enable */
-    pub oput_val: u32, /* Output Value */
-    pub pue_async: u32, /* Internal pull-up enable*/
-    pub ds: u32, /* Pin drive strength */
-    pub rise_ie: u32, /* Rise interrupt enable */
-    pub rise_ip: u32, /* Rise intr pending */
-    pub fall_ie: u32, /* Fall intr enable */
-    pub fall_ip: u32, /* Fall intr pending */
-    pub low_ie: u32,
-    pub low_ip: u32,
-    pub iof_en: u32,
-    pub iof_sel: u32,
-    pub out_xor: u32,
-    pub passthru_high_en: u32,
-    pub pthru_low_en: u32,
+ struct GpioMmapRegs{
+    iput_val: u32, /* Pin Value */
+    iput_async_en: u32, /* Pin input enable */
+    oput_en: u32, /* pin output enable */
+    oput_val: u32, /* Output Value */
+    pue_async: u32, /* Internal pull-up enable*/
+    ds: u32, /* Pin drive strength */
+    rise_ie: u32, /* Rise interrupt enable */
+    rise_ip: u32, /* Rise intr pending */
+    fall_ie: u32, /* Fall intr enable */
+    fall_ip: u32, /* Fall intr pending */
+    low_ie: u32,
+    low_ip: u32,
+    iof_en: u32,
+    iof_sel: u32,
+    out_xor: u32,
+    passthru_high_en: u32,
+    pthru_low_en: u32,
 }
+
+fn num2hexbit (num: u8) -> u32{
+    if num == 0 {
+        return 1
+    }else {
+        return 1 << num
+    }
+}
+
+ impl GpioPin {
+    #[no_mangle]
+    pub fn configure_as_in(p: u8) {
+        unsafe {
+            let mut v: u32;
+            let t = 0x1001_2000 as *mut GpioMmapRegs;
+            v = (*t).iput_async_en;
+        
+            v = v | num2hexbit(p);
+            (*t).iput_async_en = v;
+        }
+    }
+    #[no_mangle]
+    pub fn configure_as_out(p: u8) {
+        unsafe {
+            let mut v: u32;
+            let t = 0x1001_2000 as *mut GpioMmapRegs;
+            v = (*t).oput_en;
+        
+            v = v | num2hexbit(p);
+            (*t).oput_en = v;
+        }
+    }
+    #[no_mangle]
+    pub fn configure_as_io(p: u8) {
+        unsafe {
+            let mut v: u32;
+            let t = 0x1001_2000 as *mut GpioMmapRegs;
+            v = (*t).iof_en;
+        
+            v = v | num2hexbit(p);
+            (*t).iof_en = v;
+        }
+    }
+    #[no_mangle]
+    pub fn select_iof(p: u8) {
+        unsafe {
+            let mut v: u32;
+            let t = 0x1001_2000 as *mut GpioMmapRegs;
+            v = (*t).iof_sel;
+        
+            v = v | num2hexbit(p);
+            (*t).iof_sel = v;
+        }
+    }
+    #[no_mangle]
+    pub fn write_high(p: u8) {
+        unsafe {
+            let mut v: u32;
+            let t = 0x1001_2000 as *mut GpioMmapRegs;
+            v = (*t).oput_val;
+        
+            v = v | num2hexbit(p);
+            (*t).oput_val = v;
+        }
+    }
+    #[no_mangle]
+    pub fn write_low(p: u8) {
+        unsafe {
+            let mut v: u32;
+            let t = 0x1001_2000 as *mut GpioMmapRegs;
+            v = (*t).oput_val;
+            (*t).oput_val = 0;
+        
+            /* v = (*t).oput_val;
+            v = v | num2hexbit(p);
+            (*t).oput_val = v & !num2hexbit(p);
+            
+            80000080 <write_low>:
+80000080:	100125b7          	lui	a1,0x10012
+80000084:	45d0                	lw	a2,12(a1)
+80000086:	0ff57713          	zext.b	a4,a0
+8000008a:	56f9                	li	a3,-2
+8000008c:	c711                	beqz	a4,80000098 <write_low+0x18>
+8000008e:	4685                	li	a3,1
+80000090:	00a69533          	sll	a0,a3,a0
+80000094:	fff54693          	not	a3,a0
+80000098:	00d67533          	and	a0,a2,a3
+8000009c:	c5c8                	sw	a0,12(a1)
+8000009e:	8082                	ret
+
+*/
+        }
+    }
+
+ }
